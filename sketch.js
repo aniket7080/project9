9d72b47fc4e03fcad1db52ac21e8d86dd81b0051
aniket7080 <87896225+aniket7080@users.noreply.github.com>
2021-07-26 14:08:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+ var box; 
+ 
+ function setup(){
+  createCanvas(400,400)
+  
+  box = createInput();
+  box.position(150,180)
+
+  
+  
+
+  }
+
+  function draw(){
+
+  if(keyIsDown(DOWN_ARROW)){
+     
+   }
+
+   
+
+       
+
+  background("green")
+  
+}
+
+
+
+  
+
+
+
+
+
